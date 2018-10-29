63a2ea2b36398e21a413d68af291f49cfcbea7a5
benohhh <b3njaminoh@gmail.com>
2018-10-28 22:00:50 -0700

    created views folder and index.jsx

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ const app = express()
 const bodyparser = require('body-parser')
 const path = require('path')
 
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+
 app.listen(4000, _ => console.log('localhost:4000'))
---
+++ views/index.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+
+const Home = () => 
+  <html lang="en">
+    <head>
+      <meta charSet="UTF-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+      <title>Document</title>
+    </head>
+    <body>
+      
+    </body>
+  </html>

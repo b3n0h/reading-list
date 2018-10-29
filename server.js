b0f19c536774dbb7f0adfe3a10f4d9bed198c895
benohhh <b3njaminoh@gmail.com>
2018-10-28 21:40:40 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+const path = require('path')
+
+app.listen(4000, _ => console.log('localhost:4000'))

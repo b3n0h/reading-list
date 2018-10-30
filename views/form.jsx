7426215ee9655bdb5a70f17c01a9556c40ec73f4
benohhh <b3njaminoh@gmail.com>
2018-10-29 21:27:50 -0700

    created list

--- views/form.jsx
+++ views/form.jsx
@@ -1,33 +1,33 @@
 const React = require('react')
 
 const Form = _ => 
-  <div className="ui grid centered">
+  <div className="ui grid centered padded">
     <div className="eight wide column">
       <form className="ui form">
         <div className="field">
           <h1>Reading List App</h1>
           <label>Book Title</label>
-          <input type="text" name="first-name" placeholder="Jurassic Park" />
+          <input type="text" name="first-name" placeholder="Jurassic Park" id="title" />
         </div>
         <div className="field">
           <label>Book Author</label>
-          <input type="text" name="last-name" placeholder="Michael Crichton" />
+          <input type="text" name="last-name" placeholder="Michael Crichton" id="author" />
         </div>
         <div className="field">
           <label>Page Count</label>
-          <input type="text" name="last-name" placeholder="400" />
+          <input type="text" name="last-name" placeholder="400" id="pageCount" />
         </div>
         <div className="field">
           <label>Current Page Number</label>
-          <input type="text" name="last-name" placeholder="135" />
+          <input type="text" name="last-name" placeholder="135" id="currentPage" />
         </div>
         <div className="field">
           <div className="ui checkbox">
-            <input type="checkbox" name="example" />
+            <input type="checkbox" name="example" id="checkBox" />
             <label>I have read this book before</label>
           </div>
         </div>
-        <button className="ui button" type="submit">Add Book</button>
+        <button className="ui button" type="submit" id="addBook">Add Book</button>
       </form>
       <br />
       <hr />
---
+++ views/list.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+
+const BookList = _ => 
+  <html lang="en">
+  <head>
+    <meta charSet="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.css" />
+    <title>Book List</title>
+  </head>
+  <body>
+    
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.js" />
+  </body>
+  </html>

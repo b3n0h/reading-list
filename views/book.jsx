3a04f8f763f40e20ce5563fd86180ab9a498a8d1
benohhh <b3njaminoh@gmail.com>
2018-10-30 18:15:44 -0700

    fixed some things

--- views/book.jsx
+++ views/book.jsx
@@ -3,13 +3,15 @@ const Fragment = React.Fragment
 
 const Book = props =>
   <Fragment>
-    <li id="bookTitle" data-booktitle={props.title}>Title: {props.title}</li>
-    <li id="bookAuthor" data-bookauthor={props.author}>Author: {props.author}</li>
-    <li id="pageCount" data-pagecount={props.pages}>Page Count: {props.pages}</li>
-    <li id="currentPage" data-currentpage={props.currentPage}>Current Page: {props.currentPage}</li>
-    <li id="haveRead" data-haveread={props.haveRead}>{props.haveRead ? 'I have read this book' : 'I have not read this book.'}</li>
-    <button id='editBtn' data-id={props.title}>Edit</button><button id='deleteBtn' data-id={props.title}>Delete</button>
-    <br />
+    <div class="ui raised padded text container segment">
+      <h2 className="ui header" id='title' data-title={props.title}>{props.title}</h2>
+      <p id='author' data-author={props.author}>Author : {props.author}</p>
+      <p id='pageCount' data-pageCount={props.pageCount}>Page Count : {props.pageCount}</p>
+      <p id='currentPage' data-currentPage={props.currentPage}>Currently on page {props.currentPage}</p>
+      <p id='haveRead' data-haveRead={props.haveRead}>{props.haveRead ? 'I read this book already.' : 'I have not read this book yet.'}</p>
+      <button id='editBtn' data-id={props.title}>Edit</button>
+      <button id='deleteBtn' data-id={props.title}>Delete</button>
+    </div>
   </Fragment>
 
   module.exports = Book
